37d502226f20eb26461c4f16f5cf077bdf2c3738
whitehaven <accrist@me.com>
2014-07-16 14:28:49 -0600

    Began work on adding dictionary, all things look clean and good. Compartmentalizing dictionary set.

---
+++ Anagramazo/SetDictionary.h
@@ -0,0 +1,45 @@
+//
+//  SetDictionary.h
+//  Anagramazo
+//
+//  Created by Alex Crist on 7/16/14.
+//  Copyright (c) 2014 White Haven Informatics. All rights reserved.
+//
+
+#ifndef Anagramazo_Header_h
+#define Anagramazo_Header_h
+
+#include <fstream>
+#include  <set>
+
+using namespace std;
+
+set<string> createDictionary( ifstream & fin, char dictfile[] )
+{
+    set<string> Dictionary;
+    
+    fin.open( dictfile );
+    
+    string tempword;
+    
+    while ( getline( fin, tempword ) )
+    {
+        //decapitalize
+        
+        for ( int i = 0; i < tempword.length(); i++ )
+        {
+            if ( tempword[i] >= 'A' && tempword[i] <= 'Z' )
+            {
+                tempword[i] += 'a' - 'A';
+            }
+        }
+        
+        Dictionary.insert(tempword);
+    }
+    
+    fin.close();
+    
+    return Dictionary;
+}
+
+#endif
