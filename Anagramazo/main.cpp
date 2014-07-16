4addf48f0fccdb0338d6696cfeb5db0771fe0fd1
whitehaven <accrist@me.com>
2014-07-16 14:29:23 -0600

    Began work on adding dictionary, all things look clean and good. Compartmentalizing dictionary set.

---
+++ Anagramazo/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Anagramazo
+//
+//  Created by Alex Crist on 7/16/14.
+//  Copyright (c) 2014 White Haven Informatics. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include "SetDictionary.h"
+
+int main(int argc, const char * argv[])
+{
+    ifstream fin;
+    
+    
+    
+    return 0;
+}
+
